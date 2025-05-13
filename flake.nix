9797fffab5018a1f0723ce3fa55f12cac664cc2c
Leonardo <leonflorenzano@protonmail.com>
2025-05-13 14:19:14 +0200

    prima versione di home manager

--- flake.nix
+++ flake.nix
@@ -4,27 +4,25 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware }:
+  outputs = { self, nixpkgs }@inputs:
   let
     system = "x86_64-linux";
 
     pkgs = import nixpkgs {
       inherit system;
-
       config.allowUnfree = true;
     };
-  in
-  {
-    nixosConfigurations.hermes = nixpkgs.lib.nixosSystem {
-      specialArgs = {
-        inherit system;
-      };
+  in {
+    nixosConfigurations."hermes" = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs system; };
 
       modules = [
         ./nixos/configuration.nix
-        nixos-hardware.nixosModules.lenovo-thinkpad-l14-intel
       ];
     };
   };
