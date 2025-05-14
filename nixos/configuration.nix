527eecb69dc65724f8b65b0e93c7f37f511c2eb5
Leonardo <128467278+le0flo@users.noreply.github.com>
2025-05-14 11:42:09 +0200

    riaggiunto okular per i pdf

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -59,7 +59,6 @@
   services.desktopManager.plasma6.enable = true;
   environment.plasma6.excludePackages = [
     pkgs.xterm
-    pkgs.kdePackages.okular
     pkgs.kdePackages.elisa
     pkgs.kdePackages.xwaylandvideobridge
     pkgs.kdePackages.plasma-browser-integration
