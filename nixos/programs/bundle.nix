7bf79cd8324aca4e23ca55660662189c44d46dea
Leonardo <leonflorenzano@protonmail.com>
2025-05-27 11:46:44 +0200

    grande refactoring

---
+++ nixos/programs/bundle.nix
@@ -0,0 +1,27 @@
+{ ... }: {
+  imports = [
+    ./utilities.nix
+    ./dev.nix
+    ./virtual-machines.nix
+    ./games.nix
+    ./obs.nix
+  ];
+
+  # NixOS
+  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Dynamic linking
+  programs.nix-ld.enable = true;
+
+  # Base
+  utilities.enable = true;
+
+  # Work
+  dev.enablue = true;
+  virtual-machines.enable = true;
+
+  # Casual
+  games.enable = true;
+  obs.enable = true;
+}
