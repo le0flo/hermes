1f9e16b71b3e0f39c392996b671ae7d600fa8894
Leonardo <noreply@leoflo.me>
2025-05-28 14:53:28 +0200

    piccolo refactoring

--- home-manager/leo/hyprland/swww.nix
+++
@@ -1,9 +0,0 @@
-{lib, config, ...}: {
-  options = {
-    swww.enable = lib.mkEnableOption "Enables my Swww configuration";
-  };
-
-  config = lib.mkIf config.swww.enable {
-    services.swww.enable = true;
-  };
-}
