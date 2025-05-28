99974ae476d71f1447e459f54a4bbc873fab4def
Leonardo <leonflorenzano@protonmail.com>
2025-05-28 16:59:38 +0200

    aggiornato l'ambiente dev

--- nixos/programs/dev.nix
+++ nixos/programs/dev.nix
@@ -5,26 +5,37 @@
 
   config = lib.mkIf config.dev.enable {
     environment.systemPackages = with pkgs; [
+      # Editors
       zed-editor
 
+      # Compilers
       gcc
-      temurin-bin
-      python314
+      cmake
       rustup
+
+      # VMs
+      temurin-bin
+
+      # Interpreters
+      python314
       nodePackages.nodejs
       texliveBasic
+
+      # Other
+      pkg-config
+      hugo
       nil
       nixd
-      hugo
     ];
 
+    # Git
     programs.git = {
       enable = true;
+
       config = {
         init = {
           defaultBranch = "master";
         };
-
         core = {
           editor = "nvim";
         };
